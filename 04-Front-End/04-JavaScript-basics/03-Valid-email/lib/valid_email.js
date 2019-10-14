b6a8cf1be4a15153e38dd580b4b30e8746c1147c
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-10-14 18:02:11 +0200

    Js and regex

--- 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
+++ 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
@@ -1,5 +1,10 @@
 const valid = (email) => {
   // TODO: return true if the `email` string has the right pattern!
+  const string = email;
+  if (string.match(/([\w]+.[\w]+|[\w]+)@[\w]+\.[a-z]{2,3}/)) {
+    return true;
+  } return false;
 };
+
 
 module.exports = valid; // Do not remove.
