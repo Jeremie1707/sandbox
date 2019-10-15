172ab266459b35ed22dcdeaee9407f4d2f3f1922
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-10-15 14:02:20 +0200

    Unordered list

--- 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,19 @@
-const listItem = (content) => {
-  // TODO: return the proper <li> HTML tag with its content (as a string)
-};
+const listItem = content => `<li class="list-group-item">${content}</li>`;
+// TODO: return the proper <li> HTML tag with its content (as a string)
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  // return '<ul class="list-group">'
+  const string = items.map(item => listItem(item)).join('');
+  return `<ul class="list-group">${string}</ul>`;
 };
+
+
+console.log(unorderedList(['milk', 'butter', 'bread']));
+// <ul class="list-group">
+//   <li class="list-group-item">milk</li>
+//   <li class="list-group-item">butter</li>
+//   <li class="list-group-item">bread</li>
+// </ul>
 
 module.exports = { listItem, unorderedList }; // Do not remove.
