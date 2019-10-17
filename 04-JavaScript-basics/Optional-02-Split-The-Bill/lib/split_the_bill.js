ccae8b8b8319208b7d84cd822a628a54d3360f4c
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-10-17 10:41:11 +0200

    adding repo

--- 04-JavaScript-basics/Optional-02-Split-The-Bill/lib/split_the_bill.js
+++ 04-JavaScript-basics/Optional-02-Split-The-Bill/lib/split_the_bill.js
@@ -1,5 +1,17 @@
 const splitTheBill = (group) => {
   // TODO: implement the function and return an Object
+let totalSpending = 0;
+  let numberMember = 0;
+  Object.keys(group).forEach((member) => {
+    totalSpending += group[member];
+    numberMember += 1;
+  });
+  const spendingAverage = totalSpending / numberMember;
+  Object.keys(group).forEach((member) => {
+    group[member] -= spendingAverage;
+  });
+  return group;
 };
+
 
 module.exports = splitTheBill; // Do not remove this line.
