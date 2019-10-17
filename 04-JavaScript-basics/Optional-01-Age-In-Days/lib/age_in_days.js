259ae52937bb08d643e5a215ba8a6c2b060e5dd9
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-10-17 10:05:00 +0200

    age in days JS

--- 04-JavaScript-basics/Optional-01-Age-In-Days/lib/age_in_days.js
+++ 04-JavaScript-basics/Optional-01-Age-In-Days/lib/age_in_days.js
@@ -1,5 +1,8 @@
 const ageInDays = (day, month, year) => {
   // TODO: Implement the method and return an integer
+  const ageDays = Math.round(((Date.now() - new Date(year, (month - 1), day)) / 86400000));
+  console.log(ageDays);
+  return ageDays;
 };
 
 module.exports = ageInDays; // Do not remove. We need this for the spec to know about your method.
