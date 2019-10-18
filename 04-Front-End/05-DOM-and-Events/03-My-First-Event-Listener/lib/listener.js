da2a761c7475b54c424e1445ab3ebf215b7bef93
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-10-18 16:01:02 +0200

    event listener

--- 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,8 @@
 // TODO: React to a click on the button!
+const buttonx = document.querySelector('button');
+const audio = new Audio('sound.mp3');
+buttonx.addEventListener('click', (event) => {
+  buttonx.classList.add('disabled');
+  buttonx.innerText = 'Bingo!';
+  audio.play();
+});
