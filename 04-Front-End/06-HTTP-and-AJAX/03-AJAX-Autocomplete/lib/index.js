3f3c94ab907750c2ef5cde2cc28d15d2abb741ac
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-10-22 16:45:42 +0200

    JS Ajax fetch

--- 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,27 @@
 // TODO: Autocomplete the input with AJAX calls.
+// <li>Result 1 (TODO: remove)</li>
+const input = document.getElementById("search");
+const results = document.getElementById("results");
+
+
+const callApi = (inputCall) => {
+  fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${input.value}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      data.words.forEach((word) => {
+        let inpTry = "";
+        inpTry = `<li>${word}</li>`;
+        results.insertAdjacentHTML("beforeend", inpTry);
+      });
+    });
+};
+
+const displaySuggestions = (event) => {
+  results.innerHTML = "";
+  const value = input.value;
+  console.log(value);
+  callApi(input.value);
+};
+
+input.addEventListener("keyup", displaySuggestions);
