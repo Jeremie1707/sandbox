040a4192a2472c297268b053798673fe40828f5a
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-10-22 15:19:19 +0200

    callback JS

--- 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,11 @@
 // TODO: write your code here!
+const sports = document.querySelectorAll(".clickable");
+
+const displayClickedElement = (event) => {
+  console.log(event);
+  event.currentTarget.classList.add("active");
+};
+
+sports.forEach((sport) => {
+  sport.addEventListener("click", displayClickedElement);
+});
